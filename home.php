262866359bb70524a842404eceee7b1aabc9f04a
JohnKhor <0jetpistol0@gmail.com>
2018-01-13 22:51:49 +0800

    basic homepage with jumbotron and glyphicons

--- home.php
+++ home.php
@@ -6,6 +6,41 @@
 <body>
 	<?php require('partials/header.php'); ?>
 
+	<div class="jumbotron">
+		<div class="container-fluid text-center">
+			<h1>EventWeb</h1>	
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod	tempor incididunt ut labore et dolore magna aliqua.</p>	
+		</div>
+	</div>
+
+	<div class="container-fluid text-center">
+		<h2>Features</h2>
+		<hr>
+		<div class="row">
+			<div class="col-sm-4">
+				<a href="/events.php">
+					<span class="glyphicon glyphicon-search glyphicon-size"></span>
+					<h4>Browse Events</h4>					
+				</a>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+			<div class="col-sm-4">
+				<a href="/create-event.php">
+					<span class="glyphicon glyphicon-pencil glyphicon-size"></span>
+					<h4>Create Events</h4>
+				</a>				
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+			<div class="col-sm-4">
+				<a href="/events.php">
+					<span class="glyphicon glyphicon-calendar glyphicon-size"></span>
+					<h4>Attend Events</h4>
+				</a>				
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+		</div>
+	</div>
+
 	<?php require('partials/footer.php'); ?>
 	<script src="js/script.js"></script>
 </body>
