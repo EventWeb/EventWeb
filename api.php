2b247a8855e0fcc1ead8785f5d249508322b1918
JohnKhor <0jetpistol0@gmail.com>
2018-01-18 01:05:15 +0800

    now user can choose to attend or not attend an event

--- api.php
+++ api.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$_SESSION['username'] = 'john'; // Temporary hardcoded
 
 // Connect to database
 $config = require('config.php');
@@ -11,7 +13,7 @@ try {
 
 if (isset($_GET['eventName'])) {
 	// Retrieve event details based on event name
-	$sql = 'SELECT event.description, event.date, event.time, event.venue, user.name AS organizer
+	$sql = 'SELECT event.id, event.description, event.date, event.time, event.venue, user.name AS organizer
 		FROM event
 		INNER JOIN user ON event.user_id = user.id
 		WHERE event.name = :event';
@@ -21,8 +23,38 @@ if (isset($_GET['eventName'])) {
 
 	// Send response to client
 	if ($event) {
+		// Check if user is attending the event
+		if (isset($_SESSION['username'])) {
+			$sql = 'SELECT 1 
+				FROM participation 
+				WHERE event_id = :event_id AND user_id = (SELECT id FROM user WHERE name = :name)';
+			$stmt = $pdo->prepare($sql);
+			$stmt->execute(['event_id' => $event['id'], 'name' => $_SESSION['username']]);
+			$attendance = $stmt->fetch();
+
+			$event['attendance'] = !empty($attendance);
+			unset($event['id']);
+		}
 		echo json_encode($event);	
 	} else {
-		echo "ERROR: No such event.";
+		echo json_encode("ERROR: No such event.");
 	}
+} else if (isset($_POST['eventName']) && isset($_POST['eventAttendance'])) {
+	if ($_POST['eventAttendance'] === 'true') { // User doesn't want to attend, remove row
+		$_SESSION['eventAttendanceMessage'] = 'You have choose to not attend ' . $_POST['eventName'] . '.';
+		$sql = 'DELETE FROM participation
+			WHERE event_id = (SELECT id FROM event WHERE name = :eventname)
+			AND user_id = (SELECT id FROM user WHERE name = :username)';
+	} else if ($_POST['eventAttendance'] === 'false') { // User wants to attend, insert row
+		$_SESSION['eventAttendanceMessage'] = 'You have choose to attend ' . $_POST['eventName'] . '.';
+		$sql = 'INSERT INTO participation (event_id, user_id)
+			VALUES ((SELECT id FROM event WHERE name = :eventname),
+			(SELECT id FROM user WHERE name = :username))';
+	} else { // Undefined behaviour
+		die($_POST['eventAttendance']);
+	}
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(['eventname' => $_POST['eventName'], 'username' => $_SESSION['username']]);
+
+	header('location: events.php');
 }
--- js/script.js
+++ js/script.js
@@ -8,18 +8,43 @@ for (var i = 0; i < links.length; i++) {
 
 var filename = window.location.pathname;
 if (filename === '/events.php') {
-	function setEventModal(eventName, eventDetail) {
+	function setEventModal(eventName, eventDetail, isEventEnded) {
 		var modalContent = document.getElementsByClassName('modal-content')[0];
-		modalContent.getElementsByClassName('modal-title')[0].textContent = eventName;
-		if (eventDetail === 'ERROR: No such event.') {
-			return;
+
+		// Aesthetics 
+		if (eventDetail !== 'ERROR: No such event.') {
+			modalContent.getElementsByClassName('modal-title')[0].textContent = eventName;
+			modalContent.getElementsByClassName('event-organizer')[0].textContent = eventDetail['organizer'];
+			modalContent.getElementsByClassName('event-time')[0].textContent = eventDetail['time'];
+			modalContent.getElementsByClassName('event-date')[0].textContent = eventDetail['date'];
+			modalContent.getElementsByClassName('event-venue')[0].textContent = eventDetail['venue'];
+			modalContent.getElementsByClassName('event-description')[0].textContent = eventDetail['description'];
 		}
-		modalContent.getElementsByClassName('event-organizer')[0].textContent = eventDetail['organizer'];
-		modalContent.getElementsByClassName('event-time')[0].textContent = eventDetail['time'];
-		modalContent.getElementsByClassName('event-date')[0].textContent = eventDetail['date'];
-		modalContent.getElementsByClassName('event-venue')[0].textContent = eventDetail['venue'];
-		modalContent.getElementsByClassName('event-description')[0].textContent = eventDetail['description'];
+
+		// Inputs
+		if (isEventEnded) { // Hide going button
+			modalContent.getElementsByClassName('going-btn')[0].classList.add('hidden');
+		} else { // Show going button
+			modalContent.getElementsByClassName('input-event-name')[0].value = eventName;
+			var goingButton = modalContent.getElementsByClassName('going-btn')[0];
+			goingButton.classList.remove('hidden');
+			if (eventDetail['attendance']) { // Make going button blue
+				goingButton.classList.remove('btn-default');
+				goingButton.classList.add('btn-primary');
+				modalContent.getElementsByClassName('input-attendance')[0].value = true;
+			} else { // Make going button white
+				goingButton.classList.remove('btn-primary');
+				goingButton.classList.add('btn-default');
+				modalContent.getElementsByClassName('input-attendance')[0].value = false;
+			}
+		}
 	};
+
+	// Reference: https://stackoverflow.com/questions/22119673
+	function findAncestor(el, cls) {
+		while ((el = el.parentElement) && !el.classList.contains(cls));
+		return el;
+	}
 
 	function eventWellHandler(e) {
 		// Get event name
@@ -39,7 +64,8 @@ if (filename === '/events.php') {
 		xhr.open('GET', 'api.php?eventName='+eventName);
 		xhr.onload = function() {
 			if (xhr.status === 200) {
-				setEventModal(eventName, JSON.parse(xhr.responseText));
+				var isEventEnded = findAncestor(e.target, 'tab-pane').id === 'past';
+				setEventModal(eventName, JSON.parse(xhr.responseText), isEventEnded);
 			} else {
 				console.log('Request failed.  Returned status of ' + xhr.status);
 			}
