d2e962d5a9651bcd86450c58929d1a4fe52ac9fc
kirthivaasan <kirthivaasan@gmail.com>
2018-01-17 16:09:47 +0800

    updated dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,107 @@
+<?php
+session_start();
+$_SESSION['username'] = "john";
+// Connect to database
+$config = require('config.php');
+$dsn = $config['connection'] . ';dbname=' . $config['dbname'] . ';charset=' . $config['charset'];
+try {
+	$pdo = new PDO($dsn, $config['username'], $config['password'], $config['options']);
+} catch (PDOException $e) {
+	die($e->getMessage());
+}
+
+// Retrieve events and their organizers
+$sql = 'SELECT event.name, event.date, event.time, user.name AS organizer
+	FROM event
+	INNER JOIN user ON event.user_id = user.id';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$events = $stmt->fetchAll();
+
+// Separate events into upcoming and past
+$myUpcomingEvents = [];
+$myPastEvents = [];
+$myOwnEvents = [];
+
+foreach ($events as $event) {
+	// Convert 00:00:00 to 00:00 AM/PM
+	$event['time'] = date('h:i A', strtotime($event['time']));
+
+	// Check whether event's datetime >= today 
+	if (date('Y-m-d H:i:s', strtotime($event['date'] . ' ' . $event['time'])) >= date('Y-m-d H:i:s')) {
+		array_push($myUpcomingEvents, $event);
+	} else {
+		array_push($myPastEvents, $event);
+	}
+    if ($event['organizer'] == $_SESSION['username']) {
+        array_push($myOwnEvents, $event);
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<?php require('partials/html-head.php'); ?>
+    <style>
+    .well {
+        margin-bottom: 0;
+    }
+	</style>
 </head>
 <body>
 	<?php require('partials/header.php'); ?>
+    <div class="container-fluid">
+        <ul class="nav nav-tabs nav-justified">
+          <li class="active"><a href="#upcoming" data-toggle="tab">My Upcoming Events</a></li>
+          <li><a href="#past" data-toggle="tab">My Past Events</a></li>
+          <li><a href="#own" data-toggle="tab">My Events</a></li>
+        </ul>
+        <div class="tab-content container-fluid">
+            <div class="tab-pane active" id="upcoming">
+                <div class="row">
+                    <?php foreach ($myUpcomingEvents as $event) { ?>
+                        <div class="col-sm-4 well">
+                            <h4><?php echo $event['name']; ?></h4>
+                            <p>By: <?php echo $event['organizer']; ?></p>
+                            <p>Date: <?php echo $event['date']; ?></p>
+                            <p>Time: <?php echo $event['time']; ?></p>
+                        </div>
+                    <?php } ?>
+                    
+                </div>
+            </div>
 
+            <div class="tab-pane" id="past">
+                <div class="row">
+                    <?php foreach ($myPastEvents as $event) { ?>
+                        <div class="col-sm-4 well">
+                            <h4><?php echo $event['name']; ?></h4>
+                            <p>By: <?php echo $event['organizer']; ?></p>
+                            <p>Date: <?php echo $event['date']; ?></p>
+                            <p>Time: <?php echo $event['time']; ?></p>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+                
+            <div class="tab-pane" id="own">
+                <div class="row">
+                    <?php foreach ($myOwnEvents as $event) { ?>
+                        <div class="col-sm-4 well">
+                            <h4><?php echo $event['name']; ?></h4>
+                            <p>By: <?php echo $event['organizer']; ?></p>
+                            <p>Date: <?php echo $event['date']; ?></p>
+                            <p>Time: <?php echo $event['time']; ?></p>
+                        </div>
+                    <?php } ?>
+                </div>                
+            </div>
+        </div>
+    </div>
 	<?php require('partials/footer.php'); ?>
+    
 	<script src="js/script.js"></script>
 </body>
 </html>
