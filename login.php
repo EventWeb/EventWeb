369e2e95a6ba1a24d519f46a5cb917123a895b74
Alfred Loo <alfred97620@gmail.com>
2018-01-21 00:30:36 +0800

    modify signup.php and login.php, partial/header.php. add logout.php

--- login.php
+++ login.php
@@ -1,14 +1,64 @@
-<?php include('server.php'); ?>
+<?php 
+    session_start();
+    $config = require('config.php');
+    $dsn = $config['connection'] . ';dbname=' . $config['dbname'] . ';charset=' . $config['charset'];
+    try {
+        $pdo = new PDO($dsn, $config['username'], $config['password'], $config['options']);
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+
+    // variable declaration
+	$username = "";
+	$email    = "";
+	$errors = array(); 
+    $_SESSION['success'] = "";
+
+    // LOGIN USER
+	if (isset($_POST['login_user'])) {
+		$username = (isset($_POST['username']) ? $_POST['username'] : null);
+        $password = (isset($_POST['password']) ? $_POST['password'] : null);
+
+		if (empty($username)) {
+			array_push($errors, "Username is required");
+		}
+		if (empty($password)) {
+			array_push($errors, "Password is required");
+		}
+
+		if (count($errors) == 0) {
+            //get user/password combination from db
+            $sql = "SELECT password FROM user WHERE name='$username'";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $pw = $stmt->fetchAll();
+            $hash = $pw['0']['password'];
+
+            if (password_verify($password, $hash)) {
+                echo "<script>window.prompt('Login Successful');</script>";
+                $_SESSION['username'] = $username;
+                $_SESSION['success'] = "You are now logged in.";
+                header('location: home.php');
+			}else {
+				array_push($errors, "Wrong username/password combination");
+			}
+		}
+	}
+?>
+
 <!DOCTYPE html>
-<html>
+<html class="login">
 <head>
 	<?php require('partials/html-head.php'); ?>
 </head>
-<body>
+<body class="loginBody">
 	<?php require('partials/header.php'); ?>
     <div class="login_page">
+        <div class="login_title">
+            <p>LOGIN</p>
+        </div>
         <img src="images/profile.png" alt="profile icon">
-        <form method="post" action="login.php">
+        <form method="post">
             <?php include ('errors.php'); ?>
             <div class="login_form">
                 <input type="text" name="username" required autocomplete="off" autofocus>
@@ -20,9 +70,6 @@
             </div>
             <input type="submit" name="login_user" value="Sign In">
         </form>  
-        <div class="form_footer">
-            <p><a href="forgetPassword.html">Forget your password?</a></p>
-        </div>
         <div class="form_footer_reg">
             Not registered yet? Signup <a href="signup.php">here</a>
         </div>
--- partials/header.php
+++ partials/header.php
@@ -7,8 +7,12 @@
 			<li><a href="/home.php">Home</a></li>
 			<li><a href="/dashboard.php">Dashboard</a></li>
 			<li><a href="/events.php">Browse Events</a></li>
-			<li><a href="/signup.php">Sign Up</a></li>
-			<li><a href="/login.php">Login</a></li>
+			<?php if(isset($_SESSION['username'])): ?>
+				<li><a class="logout" href="/logout.php">Logout</a></li>
+			<?php else: ?>
+				<li><a href="/signup.php">Sign Up</a></li>
+				<li><a href="/login.php">Login</a></li>
+			<?php endif; ?>
 		</ul>
 	</div>
 </nav>
