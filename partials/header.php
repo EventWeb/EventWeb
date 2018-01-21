21eecca3e69ecde8940891efbf739b0ae2658193
JohnKhor <0jetpistol0@gmail.com>
2018-01-21 21:40:53 +0800

    add hamburger btn for responsive navbar

--- partials/header.php
+++ partials/header.php
@@ -2,13 +2,21 @@
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<a class="navbar-brand" href="/home.php">EventWeb</a>
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 		</div>
-		<ul class="nav navbar-nav navbar-right">
-			<li><a href="/home.php">Home</a></li>
-			<li><a href="/dashboard.php">Dashboard</a></li>
-			<li><a href="/events.php">Browse Events</a></li>
-			<li><a href="/signup.php">Sign Up</a></li>
-			<li><a href="/login.php">Login</a></li>
-		</ul>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="/home.php">Home</a></li>
+				<li><a href="/dashboard.php">Dashboard</a></li>
+				<li><a href="/events.php">Browse Events</a></li>
+				<li><a href="/signup.php">Sign Up</a></li>
+				<li><a href="/login.php">Login</a></li>
+			</ul>	
+		</div>
 	</div>
 </nav>
